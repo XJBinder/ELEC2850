d376bb66677b2ca3f10e9cddef9eb89b5c475065
XJBinder <115577036+XJBinder@users.noreply.github.com>
2023-02-19 20:36:14 -0500

    Update main.c
    Git was being lame

--- lab3Question1Grocery/lab3Question1Grocery/main.c
+++ lab3Question1Grocery/lab3Question1Grocery/main.c
@@ -9,9 +9,9 @@ void thank_you();
 
 // Main Loop
 void main() {
-	unsigned short userInput;
-	printf("\n   Enter price: ");
-	scanf("%hd", &userInput);
+	float userInput;
+	printf("\n   Enter price: $");
+	scanf("%f", &userInput);
 
 	float total = total_price(userInput);
 	float tax = est_tax(userInput);
@@ -26,7 +26,7 @@ float est_tax(unsigned short base_price) {
 	return tax;
 }
 
-float total_price(unsigned short base_price) {
+float total_price(float base_price) {
 	float tax = est_tax(base_price);  
 	float total = base_price + tax;
 	return total;
