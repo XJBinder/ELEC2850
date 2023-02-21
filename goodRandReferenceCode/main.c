7b2448e07ee8a0a173371c195290e4cfa2783ee2
XJBinder <115577036+XJBinder@users.noreply.github.com>
2023-02-20 22:39:17 -0500

    Working

--- goodRandReferenceCode/main.c
+++ goodRandReferenceCode/main.c
@@ -8,9 +8,11 @@ void main() {
 	unsigned char array[10];
 
 	for (unsigned char i = 0; i < 10; i++) {
-		quizScores[i] = (rand() % 11);  // Creates array with 10 random numbers
+		array[i] = (rand() % 11);  // Creates array with 10 random numbers
 	}
+
+	printf("\n");
 	for (unsigned char i = 0; i < 10; i++) {
-		printf("\n   %d", array[i]);  // Checks to see if it works
+		printf("   %d\n", array[i]);  // Checks to see if it works
 	}
 }
